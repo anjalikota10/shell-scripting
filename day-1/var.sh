32a5c320949d64c3090c0d83adc4e57cd85872c9
root <root@ip-172-31-6-125.ap-south-1.compute.internal>
2024-09-01 12:00:26 +0000

    variables

---
+++ day-1/var.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This is devops batch
+
+<< comment
+
+anything written in comment will not be executed
+
+comment
+
+
+name="Vertex"
+echo "This is $name Technosys and $(date)"
+
+
